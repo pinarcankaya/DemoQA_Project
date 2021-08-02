6a080ae8ab48479f0c904df77632f9b56e5a5948
pinarcankaya <67189985+pinarcankaya@users.noreply.github.com>
2021-08-03 00:44:58 +0200

    Updated

--- src/test/java/com/demoqa/pages/US18_Slider_Page.java
+++ src/test/java/com/demoqa/pages/US18_Slider_Page.java
@@ -6,17 +6,10 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class US18_Slider_Page {
-<<<<<<< HEAD
 
-    public US18_Slider_Page() {
-        PageFactory.initElements(Driver.getDriver(), this);
-    }
-
-
-=======
     public US18_Slider_Page(){
         PageFactory.initElements(Driver.getDriver(),this);}
->>>>>>> Exercices
+
     @FindBy(xpath = "(//div[@class='card-up'])[4]")
     public WebElement widgetMenuCard;
 
@@ -31,12 +24,10 @@ public class US18_Slider_Page {
 
     @FindBy(xpath = "//input[@id='sliderValue']")
     public WebElement sliderValue;
-<<<<<<< HEAD
+
 
 
     @FindBy(xpath = "//div[@class='range-slider__tooltip range-slider__tooltip--auto range-slider__tooltip--bottom']")
     public WebElement  sliderNokta;
 
-=======
->>>>>>> Exercices
 }
