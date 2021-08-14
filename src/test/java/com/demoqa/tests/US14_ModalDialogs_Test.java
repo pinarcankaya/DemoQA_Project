c7be087d58c512cf4097ea76cbc9448876deba16
Mehmet <mlalek@gmail.com>
2021-08-14 21:37:49 +0200

    updated US14

--- src/test/java/com/demoqa/tests/US14_ModalDialogs_Test.java
+++ src/test/java/com/demoqa/tests/US14_ModalDialogs_Test.java
@@ -12,11 +12,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
-//Not: 72 ve 91 satirlar calismiyor
+
 
 public class US14_ModalDialogs_Test {
 
@@ -32,11 +31,11 @@ public class US14_ModalDialogs_Test {
         Driver.getDriver().manage().window().maximize();
         Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         this.actions.sendKeys(Keys.PAGE_DOWN).perform();
-       // ((JavascriptExecutor)Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", this.modalDialogsPage.alertsFrameWindows);
-        this.modalDialogsPage.alertsFrameWindows.click();
+        ReusableMethods.clickStaleElement(modalDialogsPage.alertsFrameWindows);
         this.actions.sendKeys(Keys.PAGE_DOWN).perform();
         ((JavascriptExecutor)Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", this.modalDialogsPage.modalDialogs);
         this.modalDialogsPage.modalDialogs.click();
+
     }
     //TC063
     //1- Click "Small Modal" button
@@ -47,11 +46,8 @@ public class US14_ModalDialogs_Test {
 
     @Test (priority = 2, dependsOnMethods = {"modalDialogs"})
     public void modalDiaButtons()  {
-        //this.wait.until(ExpectedConditions.visibilityOf(modalDialogsPage.smallButton));
-       //((JavascriptExecutor)Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", this.modalDialogsPage.smallButton);
-        ReusableMethods.waitFor(2);
         this.modalDialogsPage.smallButton.click();
-        ReusableMethods.waitFor(2);
+        ReusableMethods.waitFor(1);
         Assert.assertTrue(modalDialogsPage.text.isDisplayed());
 
         this.wait.until(ExpectedConditions.visibilityOf(modalDialogsPage.smallLargeClose));
@@ -61,6 +57,8 @@ public class US14_ModalDialogs_Test {
         this.modalDialogsPage.largeButton.click();
 
         Assert.assertTrue(modalDialogsPage.text.isDisplayed());
+        this.modalDialogsPage.smallLargeClose.click();
+        //ReusableMethods.waitFor(1);
     }
 
     //TC064
@@ -68,9 +66,8 @@ public class US14_ModalDialogs_Test {
     //2- Verify that text of the Modal dialog box/pupup has 47 characters.
     @Test (priority = 3, dependsOnMethods = {"modalDialogs"})
     public void textCharSize() {
-        ReusableMethods.waitFor(2);
-        this.modalDialogsPage.smallButton.click();
-        ReusableMethods.waitFor(2);
+        ReusableMethods.clickWithJS(modalDialogsPage.smallButton);
+        ReusableMethods.waitFor(1);
         int lengthOfText = modalDialogsPage.text.getText().length();
 
         System.out.println(modalDialogsPage.text.getText());
@@ -78,6 +75,7 @@ public class US14_ModalDialogs_Test {
         Assert.assertEquals(lengthOfText ,47);
 
         this.modalDialogsPage.smallLargeClose.click();
+        //ReusableMethods.waitFor(1);
     }
 
     //TC065
@@ -87,31 +85,32 @@ public class US14_ModalDialogs_Test {
 
     @Test (priority = 4, dependsOnMethods = {"modalDialogs"})
     public void verifyClose()  {
-        ReusableMethods.waitFor(3);
-        this.modalDialogsPage.smallButton.click();
-        ReusableMethods.waitFor(2);
+
+        ReusableMethods.clickStaleElement(modalDialogsPage.smallButton);
+        ReusableMethods.waitFor(1);
 
         // 1.yol: Dialog penceresindeki close button un "size"  "1" oldugunu ve kapandiktan sonra da buton
         // gorunmedigi icin "size"  "0" oldugunu dogrula.
         // Not:size() ini alabilmek icin elementi(closeLarge2) List icine aldik
 
         System.out.println(modalDialogsPage.closeSmall.size());
-        ReusableMethods.waitFor(2);
+        ReusableMethods.waitFor(1);
         this.modalDialogsPage.smallClose.click();
-        ReusableMethods.waitFor(2);
+        ReusableMethods.waitFor(1);
 
         Assert.assertEquals(modalDialogsPage.closeSmall.size(), 0);
         System.out.println(modalDialogsPage.closeSmall.size());
 
-//        2.yol: butonu false / true yaparak assert yap
+//        2.yol: butonu false / true yaparak assert yap.
         this.modalDialogsPage.smallButton.click();
-        ReusableMethods.waitFor(2);
+        ReusableMethods.waitFor(1);
         this.modalDialogsPage.smallClose.click();
-        ReusableMethods.waitFor(2);
+        ReusableMethods.waitFor(1);
 
-        boolean button = ReusableMethods.isElementVisible(modalDialogsPage.smallClose);
+        boolean button = ReusableMethods.isElementVisible(modalDialogsPage.smallClose); //button visible mi? degil, degilse false olsun
         Assert.assertFalse(button);
         System.out.println("result = " + button);
+
     }
 
     //TC066
@@ -119,9 +118,7 @@ public class US14_ModalDialogs_Test {
     //2-Verify that the text has font-family "Roboto" in Modal dialog box
     @Test (priority = 5, dependsOnMethods = {"modalDialogs"})
     public void fontFamily(){
-        ReusableMethods.waitFor(3);
-        modalDialogsPage.largeButton.click();
-
+        ReusableMethods.clickStaleElement(modalDialogsPage.largeButton);
         String bodyFont = modalDialogsPage.text.getCssValue("font-family");
         System.out.println(bodyFont);
         Assert.assertTrue(bodyFont.contains("Roboto"));
@@ -130,6 +127,8 @@ public class US14_ModalDialogs_Test {
             System.out.println("Font family contains: Roboto");
         }else
         System.out.println("Font family does not contain: Roboto");
+        this.modalDialogsPage.smallLargeClose.click();
+
     }
 
 
@@ -140,10 +139,7 @@ public class US14_ModalDialogs_Test {
 
     @Test (priority = 6, dependsOnMethods = {"modalDialogs"})
     public void assertClose() {
-        ReusableMethods.waitFor(4);
-        //((JavascriptExecutor)Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", this.modalDialogsPage.buttons);
-        this.modalDialogsPage.buttons.click();
-
+        ReusableMethods.clickStaleElement(modalDialogsPage.buttons);
 
         // 1.yol: Dialog penceresindeki close button un "size"  "1" oldugunu ve kapandiktan sonra da buton
         // gorunmedigi icin "size"  "0" oldugunu dogrula.
